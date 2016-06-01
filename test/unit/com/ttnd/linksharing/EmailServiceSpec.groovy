e0b436b64aaef9f9ba09e5757af800fc483368c3
Shalika Singhal <shalika.singhal@tothenew.com>
2016-06-01 22:33:11 +0530

    User service testCase written

--- test/unit/com/ttnd/linksharing/EmailServiceSpec.groovy
+++ test/unit/com/ttnd/linksharing/EmailServiceSpec.groovy
@@ -1,7 +1,12 @@
 package com.ttnd.linksharing
 
+import com.ttnd.linksharing.DTO.EmailDTO
 import grails.test.mixin.TestFor
+import org.codehaus.groovy.tools.shell.util.MessageSource
 import spock.lang.Specification
+
+import javax.validation.constraints.Null
+
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
@@ -9,5 +14,18 @@ import spock.lang.Specification
 @TestFor(EmailService)
 class EmailServiceSpec extends Specification {
 
+
+    def "send Unread Items mail To user"()
+    {
+        given:
+        def mockedMessageSource=Mock(MessageSource)
+        service.messageSource=mockedMessageSource
+
+        when:
+       EmailDTO emailDTO= service.sendUnreadResourcesEmail(new User(),[new LinkResource()])
+        then:
+        emailDTO!=Null
+
+    }
 
 }
--- test/unit/com/ttnd/linksharing/UserServiceSpec.groovy
+++ test/unit/com/ttnd/linksharing/UserServiceSpec.groovy
@@ -10,4 +10,19 @@ import spock.lang.Specification
 class UserServiceSpec extends Specification {
 
 
+     def "send Unread Items Email"()
+     {
+         given:
+         def mockedEmailService=Mock(EmailService)
+         service.emailService=mockedEmailService
+         service.metaClass.getUserWithUnreadItems ={-> [new User()]}
+         service.metaClass. getUnreadResources ={User user -> [new LinkResource()]}
+
+         when:
+         service.sendUnreadItemsEmail()
+         then:
+         1*mockedEmailService.sendUnreadResourcesEmail(_,_)
+
+     }
+
 }
